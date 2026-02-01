6f625bb19119a5f82a0755628577ca3cbd66902a
Gregor Debus <accounts@gregordebus.de>
2026-02-01 15:28:25 +0100

    fixed example pipelines

--- agents/input/example_pipelines/example_pipeline.py
+++ agents/input/example_pipelines/example_pipeline.py
@@ -4,22 +4,11 @@
 
 from PyDI.io import load_parquet, load_csv, load_xml
 
-from PyDI.entitymatching import (
-    StandardBlocker,
-    EmbeddingBlocker,
-    SortedNeighbourhoodBlocker,
-    TokenBlocker,
-)
+from PyDI.entitymatching import StandardBlocker, EmbeddingBlocker, SortedNeighbourhoodBlocker, TokenBlocker
 from PyDI.entitymatching import StringComparator, NumericComparator
 from PyDI.entitymatching import RuleBasedMatcher
 
-from PyDI.fusion import (
-    DataFusionStrategy,
-    DataFusionEngine,
-    longest_string,
-    union,
-    prefer_higher_trust,
-)
+from PyDI.fusion import DataFusionStrategy, DataFusionEngine, longest_string, union, prefer_higher_trust
 from PyDI.fusion import DataFusionEvaluator, tokenized_match
 
 from PyDI.schemamatching import LLMBasedSchemaMatcher
@@ -49,7 +38,7 @@ good_dataset_name_1 = load_parquet(
     name="dataset_name_1",
 )
 
-# Load Uber Eats dataset
+# Load Uber Eats dataset  
 good_dataset_name_2 = load_parquet(
     DATA_DIR + "<path-and-file-name-of-dataset-2>.parquet",
     name="dataset_name_2",
@@ -60,6 +49,7 @@ good_dataset_name_3 = load_parquet(
     DATA_DIR + "<path-and-file-name-of-dataset-3>.parquet",
     name="dataset_name_3",
 )
+
 
 
 datasets = [good_dataset_name_1, good_dataset_name_2, good_dataset_name_3]
@@ -79,20 +69,28 @@ llm = ChatOpenAI(
     max_tokens=None,
 )
 
-matcher = LLMBasedSchemaMatcher(chat_model=llm, num_rows=10, debug=True)
+matcher = LLMBasedSchemaMatcher(
+    chat_model=llm,
+    num_rows=10,
+    debug=True
+)
 
 # match schema of good_dataset_name_1 with good_dataset_name_2 and rename schema of good_dataset_name_2
 schema_correspondences = matcher.match(good_dataset_name_1, good_dataset_name_2)
-rename_map = schema_correspondences.set_index("target_column")[
-    "source_column"
-].to_dict()
+rename_map = (
+    schema_correspondences
+    .set_index("target_column")["source_column"]
+    .to_dict()
+)
 good_dataset_name_2 = good_dataset_name_2.rename(columns=rename_map)
 
 # match schema of good_dataset_name_1 with good_dataset_name_3 and rename schema of good_dataset_name_3
 schema_correspondences = matcher.match(good_dataset_name_1, good_dataset_name_3)
-rename_map = schema_correspondences.set_index("target_column")[
-    "source_column"
-].to_dict()
+rename_map = (
+    schema_correspondences
+    .set_index("target_column")["source_column"]
+    .to_dict()
+)
 good_dataset_name_3 = good_dataset_name_3.rename(columns=rename_map)
 
 # --------------------------------
@@ -103,7 +101,7 @@ good_dataset_name_3 = good_dataset_name_3.rename(columns=rename_map)
 print("Performing Blocking")
 
 # Embedding blocker example:
-# embedding_blocker_dataset1_2_dataset2 = EmbeddingBlocker(
+#embedding_blocker_dataset1_2_dataset2 = EmbeddingBlocker(
 #    good_dataset_name_1, good_dataset_name_2, # name of the datasets
 #    text_cols=['city'], # column which should be used to perform the blocking on
 #    model="sentence-transformers/all-MiniLM-L6-v2",
@@ -112,19 +110,19 @@ print("Performing Blocking")
 #    batch_size=1000,
 #    output_dir="output/blocking-evaluation",
 #    id_column='id'
-# )
+#)
 
 # Sorted NeighbourhoodBlocker example
-# blocker = SortedNeighbourhoodBlocker(
+#blocker = SortedNeighbourhoodBlocker(
 #    df_left, df_right,
 #    key="name",
 #    window=20,
 #    id_column="id",
 #    output_dir="output/blocking"
-# )
+#)
 
 # TokenBlocker example
-# blocker = TokenBlocker(
+#blocker = TokenBlocker(
 #    df_left, df_right,
 #    column="name",
 #    min_token_len=3,
@@ -132,34 +130,31 @@ print("Performing Blocking")
 #    ngram_type="word",
 #    id_column="id",
 #    output_dir="output/blocking"
-# )
+#)
 
 # Standard blocker example:
 blocker_k2u = StandardBlocker(
-    good_dataset_name_1,
-    good_dataset_name_2,
-    on=["city"],
+    good_dataset_name_1, good_dataset_name_2,
+    on=['city'],
     batch_size=1000,
     output_dir="output/blocking-evaluation",
-    id_column="id",
+    id_column='id'
 )
 
 blocker_k2y = StandardBlocker(
-    good_dataset_name_1,
-    good_dataset_name_3,
-    on=["city"],
+    good_dataset_name_1, good_dataset_name_3,
+    on=['city'],
     batch_size=1000,
     output_dir="output/blocking-evaluation",
-    id_column="id",
+    id_column='id'
 )
 
 blocker_u2y = StandardBlocker(
-    good_dataset_name_2,
-    good_dataset_name_3,
-    on=["city"],
+    good_dataset_name_2, good_dataset_name_3,
+    on=['city'],
     batch_size=1000,
     output_dir="output/blocking-evaluation",
-    id_column="id",
+    id_column='id'
 )
 
 # --------------------------------
@@ -169,82 +164,91 @@ blocker_u2y = StandardBlocker(
 comparators_k2u = [
     # Name similarity
     StringComparator(
-        column="name_norm",
-        similarity_function="jaccard",
+        column='name_norm',
+        similarity_function='jaccard', 
         # no preprocessing needed
     ),
+    
     # street name similarity
     StringComparator(
-        column="street",
-        similarity_function="jaccard",
+        column='street',
+        similarity_function='jaccard', 
         preprocess=str.lower,
     ),
+
     # house number similarity
     NumericComparator(
-        column="house_number",
+        column='house_number',
         max_difference=2,
     ),
+
     # category similarity
     StringComparator(
-        column="categories",
-        similarity_function="jaccard",
+        column='categories',
+        similarity_function='jaccard',
         preprocess=str.lower,
-        list_strategy="concatenate",  # Handle list attribute by concatenation
-    ),
+        list_strategy='concatenate' # Handle list attribute by concatenation
+    )
 ]
 
 comparators_k2y = [
     # Name similarity
     StringComparator(
-        column="name_norm",
-        similarity_function="jaccard",
+        column='name_norm',
+        similarity_function='jaccard', 
         # no preprocessing needed
     ),
+    
     # street name similarity
     StringComparator(
-        column="street",
-        similarity_function="jaccard",
+        column='street',
+        similarity_function='jaccard', 
         preprocess=str.lower,
     ),
+
     # house number similarity
     NumericComparator(
-        column="house_number",
+        column='house_number',
         max_difference=2,
     ),
+
     # category similarity
     StringComparator(
-        column="categories",
-        similarity_function="jaccard",
+        column='categories',
+        similarity_function='jaccard',
         preprocess=str.lower,
-        list_strategy="concatenate",  # Handle list attribute by concatenation
-    ),
+        list_strategy='concatenate' # Handle list attribute by concatenation
+    )
 ]
 
 comparators_u2y = [
     # Name similarity
     StringComparator(
-        column="name_norm",
-        similarity_function="jaccard",
+        column='name_norm',
+        similarity_function='jaccard', 
         # no preprocessing needed
     ),
+    
     # street name similarity
     StringComparator(
-        column="street",
-        similarity_function="jaccard",
+        column='street',
+        similarity_function='jaccard', 
         preprocess=str.lower,
     ),
+
     # house number similarity
     NumericComparator(
-        column="house_number",
+        column='house_number',
         max_difference=2,
     ),
+
     # category similarity
     StringComparator(
-        column="categories",
-        similarity_function="jaccard",
+        column='categories',
+        similarity_function='jaccard',
         preprocess=str.lower,
-        list_strategy="concatenate",  # Handle list attribute by concatenation
-    ),
+        list_strategy='concatenate' # Handle list attribute by concatenation
+    )
 ]
 
 print("Matching Entities")
@@ -254,67 +258,32 @@ matcher = RuleBasedMatcher()
 
 rb_correspondences_k2u = matcher.match(
     df_left=good_dataset_name_1,
-    df_right=good_dataset_name_2,
+    df_right=good_dataset_name_2, 
     candidates=blocker_k2u,
     comparators=comparators_k2u,
-    weights=[
-        0.5,
-        0.2,
-        0.2,
-        0.1,
-    ],  # weight the different comparators. Adjust accordingly so they make sense
+    weights=[0.5, 0.2, 0.2, 0.1], # weight the different comparators. Adjust accordingly so they make sense
     threshold=0.7,
-    id_column="id",
+    id_column='id'
 )
 
 rb_correspondences_k2y = matcher.match(
     df_left=good_dataset_name_1,
-    df_right=good_dataset_name_3,
+    df_right=good_dataset_name_3, 
     candidates=blocker_k2y,
     comparators=comparators_k2y,
-    weights=[0.5, 0.2, 0.2, 0.1],
+    weights=[0.5, 0.2, 0.2, 0.1],  
     threshold=0.7,
-    id_column="id",
+    id_column='id'
 )
 
 rb_correspondences_u2y = matcher.match(
     df_left=good_dataset_name_2,
-    df_right=good_dataset_name_3,
+    df_right=good_dataset_name_3, 
     candidates=blocker_u2y,
     comparators=comparators_u2y,
-    weights=[0.5, 0.2, 0.2, 0.1],
+    weights=[0.5, 0.2, 0.2, 0.1],  
     threshold=0.7,
-    id_column="id",
-)
-
-"""
-Make sure to save correspondences for each pair afer applying Matcher.
-**Use proper filename with correct dataset name to save the correspondences**
-"""
-
-CORR_DIR = "output/correspondences"
-os.makedirs(CORR_DIR, exist_ok=True)
-
-rb_correspondences_k2u.to_csv(
-    os.path.join(
-        CORR_DIR,
-        "correspondences_good_dataset_name_1_good_dataset_name_2.csv",
-        index=False,
-    )
-)
-rb_correspondences_k2y.to_csv(
-    os.path.join(
-        CORR_DIR,
-        "correspondences_good_dataset_name_1_good_dataset_name_3.csv",
-        index=False,
-    )
-)
-rb_correspondences_u2y.to_csv(
-    os.path.join(
-        CORR_DIR,
-        "correspondences_good_dataset_name_2_good_dataset_name_3.csv",
-        index=False,
-    )
+    id_column='id'
 )
 
 print("Fusing Data")
@@ -329,32 +298,24 @@ print("Fusing Data")
 # --------------------------------
 
 # merge rule based correspondences
-all_rb_correspondences = pd.concat(
-    [rb_correspondences_k2u, rb_correspondences_k2y, rb_correspondences_u2y],
-    ignore_index=True,
-)
+all_rb_correspondences = pd.concat([rb_correspondences_k2u, rb_correspondences_k2y, rb_correspondences_u2y], ignore_index=True)
 
 # define data fusion strategy
-strategy = DataFusionStrategy("rule_based_fusion_strategy")
+strategy = DataFusionStrategy('rule_based_fusion_strategy')
 
-strategy.add_attribute_fuser("name", longest_string)
-strategy.add_attribute_fuser("street", longest_string)
-strategy.add_attribute_fuser("house_number", longest_string)
-strategy.add_attribute_fuser("city", longest_string)
-strategy.add_attribute_fuser("state", longest_string)
-strategy.add_attribute_fuser("postal_code", longest_string)
-strategy.add_attribute_fuser("country", longest_string)
-strategy.add_attribute_fuser("latitude", longest_string)
-strategy.add_attribute_fuser("longitude", longest_string)
-strategy.add_attribute_fuser("categories", union)
+strategy.add_attribute_fuser('name', longest_string)
+strategy.add_attribute_fuser('street', longest_string)
+strategy.add_attribute_fuser('house_number', longest_string)
+strategy.add_attribute_fuser('city', longest_string)
+strategy.add_attribute_fuser('state', longest_string)
+strategy.add_attribute_fuser('postal_code', longest_string)
+strategy.add_attribute_fuser('country', longest_string)
+strategy.add_attribute_fuser('latitude', longest_string)
+strategy.add_attribute_fuser('longitude', longest_string)
+strategy.add_attribute_fuser('categories', union)
 
 # run fusion
-engine = DataFusionEngine(
-    strategy,
-    debug=True,
-    debug_format="json",
-    debug_file="output/data_fusion/debug_fusion_data.jsonl",
-)
+engine = DataFusionEngine(strategy, debug=True, debug_format='json',debug_file= "output/data_fusion/debug_fusion_data.jsonl")
 
 # fuse rule based matches
 rb_fused_standard_blocker = engine.run(
